6df0cf440c4a787e425a4e615d5512eeedd89400
winjeg <winjeg@qq.com>
2019-08-23 14:44:37 +0800

    to be comptable with postgres for the grammar is different

--- settings.go
+++ settings.go
@@ -14,12 +14,19 @@ import (
 )
 
 const (
-	getSql                 = "SELECT value FROM settings WHERE name = ?"
-	updateSql              = "UPDATE settings SET value = ? WHERE name = ?"
-	existSql               = "SELECT COUNT(*) FROM settings WHERE name= ?"
-	addSql                 = "INSERT IGNORE INTO settings(name, value) VALUE(?, ?)"
-	addSqlWithId           = "INSERT IGNORE INTO settings(id, name, value) VALUE(?, ?, ?)"
-	deleteVarSql           = "DELETE FROM settings WHERE name = ?"
+	getSql         = "SELECT value FROM settings WHERE name = ?"
+	getSqlPg       = "SELECT value FROM settings WHERE name = $1"
+	updateSql      = "UPDATE settings SET value = ? WHERE name = ?"
+	updateSqlPg    = "UPDATE settings SET value = $1 WHERE name = $2"
+	existSql       = "SELECT COUNT(*) FROM settings WHERE name= ?"
+	existSqlPg     = "SELECT COUNT(*) FROM settings WHERE name= $1"
+	addSql         = "INSERT IGNORE INTO settings(name, value) VALUE(?, ?)"
+	addSqlPg       = "INSERT IGNORE INTO settings(name, value) VALUES($1, $2)"
+	addSqlWithId   = "INSERT IGNORE INTO settings(id, name, value) VALUE(?, ?, ?)"
+	addSqlWithIdPg = "INSERT IGNORE INTO settings(id, name, value) VALUES($1, $2, $3)"
+	deleteVarSql   = "DELETE FROM settings WHERE name = ?"
+	deleteVarSqlPg = "DELETE FROM settings WHERE name = $1"
+
 	settingsSql            = `SELECT 1 FROM settings`
 	settingVar             = "1"
 	createSettingsTableSql = "CREATE TABLE `settings` ( `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'pk', `name` varchar(200) COLLATE utf8_bin NOT NULL COMMENT 'varname', `value` text COLLATE utf8_bin NOT NULL, PRIMARY KEY (`id`), UNIQUE KEY `name_UNIQUE` (`name`), KEY `idx_name` (`name`)) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_bin"
@@ -33,18 +40,20 @@ var (
 	logger              = log.GetLogger(nil)
 	db          *sql.DB = nil
 	withId              = false
+	postgres            = false
 )
 
 // generate primary key, with this function
 // if using databases that won't automatically generated primary key
 // this function may suit you, but you must make the primary key at lease 8 byte long
 
-func InitV2(dbConn *sql.DB, autoGenerateId bool) {
+func InitV2(dbConn *sql.DB, autoGenerateId, pg bool) {
 	err := Init(dbConn)
 	if err != nil {
 		return
 	}
 	withId = autoGenerateId
+	postgres = pg
 }
 
 func Init(dbConn *sql.DB) error {
@@ -90,11 +99,20 @@ func GetVar(name string) string {
 		return v
 	} else {
 		var x string
-		r := db.QueryRow(getSql, name)
-		err := r.Scan(&x)
-		if err != nil {
-			return ""
+		if postgres {
+			r := db.QueryRow(getSqlPg, name)
+			err := r.Scan(&x)
+			if err != nil {
+				return ""
+			}
+		} else {
+			r := db.QueryRow(getSql, name)
+			err := r.Scan(&x)
+			if err != nil {
+				return ""
+			}
 		}
+
 		var lock sync.Mutex
 		lock.Lock()
 		settingsMap[name] = x
@@ -109,23 +127,62 @@ func SetVar(name, value string) {
 	lock.Lock()
 	defer lock.Unlock()
 	settingsMap[name] = value
-	row := db.QueryRow(existSql, name)
+	var row *sql.Row
+	if postgres {
+		row = db.QueryRow(existSqlPg, name)
+	} else {
+		row = db.QueryRow(existSql, name)
+	}
+
 	var exists int
 	if err := row.Scan(&exists); err == nil && exists == 0 {
 		var err error
 		if withId {
 			id := uid.NextID()
-			_, err = db.Exec(addSqlWithId, id, name, value)
+			if postgres {
+				stmt, err := db.Prepare(addSqlWithIdPg)
+				if err != nil || stmt == nil {
+					logger.Error(err)
+				}
+				defer stmt.Close()
+				_, err = stmt.Exec(id, name, value)
+			} else {
+				_, err = db.Exec(addSqlWithId, id, name, value)
+			}
+
 		} else {
-			_, err = db.Exec(addSql, name, value)
+			if postgres {
+				stmt, err2 := db.Prepare(addSqlPg)
+				if err2 != nil || stmt == nil {
+					return
+				}
+				_, err = stmt.Exec(name, value)
+				defer stmt.Close()
+			} else {
+				_, err = db.Exec(addSql, name, value)
+			}
 		}
 		if err != nil {
 			logger.Error(err)
 		}
 	} else {
-		_, err = db.Exec(updateSql, value, name)
-		if err != nil {
-			logger.Error(err)
+		if postgres {
+			stmt, err := db.Prepare(updateSqlPg)
+			if err != nil {
+				logger.Error(err)
+				return
+			}
+			_, execErr := stmt.Exec(value, name)
+			if execErr != nil {
+				logger.Error(execErr)
+			}
+			defer stmt.Close()
+
+		} else {
+			_, err = db.Exec(updateSql, value, name)
+			if err != nil {
+				logger.Error(err)
+			}
 		}
 	}
 }
@@ -135,7 +192,18 @@ func DelVar(name string) {
 	lock.Lock()
 	defer lock.Unlock()
 	delete(settingsMap, name)
-	_, err := db.Exec(deleteVarSql, name)
+	var err error
+	if postgres {
+		stmt, err := db.Prepare(deleteVarSqlPg)
+		if err != nil {
+			logger.Error(err)
+			return
+		}
+		defer stmt.Close()
+		_, err = stmt.Exec(name)
+	} else {
+		_, err = db.Exec(deleteVarSql, name)
+	}
 	if err != nil {
 		logger.Error(err)
 	}
